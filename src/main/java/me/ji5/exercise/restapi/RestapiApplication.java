194d7ec8fdb3add1a6349d7b699ed96700fbe835
Jongin Oh <ohjongin@gmail.com>
2017-10-07 13:48:27 +0900

    Intial commit

---
+++ src/main/java/me/ji5/exercise/restapi/RestapiApplication.java
@@ -0,0 +1,12 @@
+package me.ji5.exercise.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+}
